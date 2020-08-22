5a6997b3c5c29387a4a3d9520c4a09c6a73665c0
Sientuo <58808100+Sientuo@users.noreply.github.com>
2020-08-22 21:29:04 +0800

    更新

--- TestPlay/Controllers/WeChatController.cs
+++ TestPlay/Controllers/WeChatController.cs
@@ -18,9 +18,6 @@ namespace TestPlay.Controllers
     {
         // GET: WeChat
         //回复消息
-        //public string AppId = "wxa6081d07acd03286";
-        //public string Token = "srgtrh4561sdf";
-        //public string EncodingAesKey = "BxiNRl4L33KAdymESW6vSBiwayezcgcXRsvNH9HBKkf";
 
         [HttpGet]
         [ActionName("Index")]
